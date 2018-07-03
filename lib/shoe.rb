88e2c76da8bc2a42d53f685c9c5edab7e8cf6bb1
Justine Bricknell <justinegayton@gmail.com>
2018-07-03 13:57:53 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,8 +6,8 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    #BRANDS << brand unless BRANDS.include?(brand)
-    BRANDS << brand unless BRANDS.any? { |x| x == brand }
+    BRANDS << brand unless BRANDS.include?(brand)
+    #alt option: BRANDS << brand unless BRANDS.any? { |x| x == brand }
   end
 
   def cobble
